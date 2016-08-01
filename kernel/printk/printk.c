9f53ab72e7679d17dcd0b5a9ca493b597e07a52e
andip71 <andreasp@gmx.de>
2016-08-01 11:37:29 +0200

    printk: Add sys kernel interface to configure linux printk logging
    - now logging can be switched on and off via /sys/kernel/printk_mode interface
    - default is 1 (on)
    - Enabled time stamp information in logs again
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -53,6 +53,8 @@
 #include <asm/uaccess.h>
 #include <asm/cputype.h>
 
+#include "../printk_interface.h"
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -1856,6 +1858,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 	/* cpu currently holding logbuf_lock in this function */
 	static unsigned int logbuf_cpu = UINT_MAX;
 
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+			return 0;
+
 	if (level == LOGLEVEL_SCHED) {
 		level = LOGLEVEL_DEFAULT;
 		in_sched = true;
@@ -2026,6 +2032,10 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+			return 0;
+
 	return vprintk_emit(0, LOGLEVEL_DEFAULT, NULL, 0, fmt, args);
 }
 EXPORT_SYMBOL(vprintk);
@@ -2036,6 +2046,10 @@ asmlinkage int printk_emit(int facility, int level,
 {
 	va_list args;
 	int r;
+
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+			return 0;
 
 	va_start(args, fmt);
 	r = vprintk_emit(facility, level, dict, dictlen, fmt, args);
@@ -2048,6 +2062,10 @@ EXPORT_SYMBOL(printk_emit);
 int vprintk_default(const char *fmt, va_list args)
 {
 	int r;
+
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+			return 0;
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
@@ -2095,6 +2113,10 @@ asmlinkage __visible int printk(const char *fmt, ...)
 	printk_func_t vprintk_func;
 	va_list args;
 	int r;
+
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+		return 0;
 
 	va_start(args, fmt);
 
