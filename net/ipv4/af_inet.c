c799a27cb4cc1a5fb34370d32c469154ab92396f
djb77 <dwayne.bakewell@gmail.com>
2017-07-08 23:29:56 +1000

    ipv4: Disable KNOX_NCM
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -419,6 +419,7 @@ out_rcu_unlock:
 	goto out;
 }
 
+#ifdef CONFIG_KNOX_NCM
 /** The function is used to check if the ncm feature is enabled or not; if enabled then collect the socket meta-data information; **/
 static void knox_collect_metadata(struct socket *sock) {
     if(check_ncm_flag()) {
@@ -477,6 +478,7 @@ static void knox_collect_metadata(struct socket *sock) {
         }
     }
 }
+#endif
 
 /*
  *	The peer socket should always be NULL (or else). When we call this
@@ -504,7 +506,9 @@ int inet_release(struct socket *sock)
 		if (sock_flag(sk, SOCK_LINGER) &&
 		    !(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
+#ifdef CONFIG_KNOX_NCM
         knox_collect_metadata(sock);
+#endif
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
 	}
