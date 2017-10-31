c5f64ee74d63330307815ea910967952c7c48cf8
Tkkg1994 <luca@grifodev.ch>
2017-10-31 12:45:14 +1100

    staging/samsung: linux 4.4.95 introduced a similar key_stated
    Rename to fix compiling
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/staging/samsung/sec_upload.c
+++ drivers/staging/samsung/sec_upload.c
@@ -44,12 +44,12 @@ struct crash_key user_crash_key_combination[] = {
 	{KEY_POWER, CRASH_COUNT_THIRD},
 };
 
-struct key_state {
+struct key_stated {
 	unsigned int key_code;
 	unsigned int state;
 };
 
-struct key_state key_states[] = {
+struct key_stated key_states[] = {
 	{KEY_VOLUMEDOWN, KEY_STATE_UP},
 	{KEY_VOLUMEUP, KEY_STATE_UP},
 	{KEY_POWER, KEY_STATE_UP},
