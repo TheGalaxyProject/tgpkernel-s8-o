18ad33c6cb7e0cb452f382d5ba3e7109391ce6c6
djb77 <dwayne.bakewell@gmail.com>
2018-01-11 00:30:54 +1100

    scripts: Modify fetch-latest-wireguard.sh
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+FETCH_WIREGUARD()
+{
 set -e
 USER_AGENT="WireGuard-AndroidROMBuild/0.1 ($(uname -a))"
 
@@ -16,3 +19,11 @@ mkdir -p net/wireguard
 curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-${BASH_REMATCH[1]}/src"
 sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
 touch net/wireguard/.check
+}
+
+cd $WORKDIR
+FETCH_WIREGUARD
+
+cd $RDIR
+FETCH_WIREGUARD
+
