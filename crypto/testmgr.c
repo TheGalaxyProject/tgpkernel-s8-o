92262eba4b227a6dbda30fb1bea6337c040b0692
djb77 <dwayne.bakewell@gmail.com>
2018-01-23 10:32:18 +1100

    LINUX: 4.4.66 Kernel Update
    Conflicts:
          fs/f2fs/super.c
          net/packet/af_packet.c
    
    NOTE: Did not patch super.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -548,7 +548,6 @@ static int __test_aead(struct crypto_aead *tfm, int enc,
 
 		memcpy(input, template[i].input, template[i].ilen);
 		memcpy(assoc, template[i].assoc, template[i].alen);
-
 		if (template[i].iv)
 			memcpy(iv, template[i].iv, iv_len);
 		else
